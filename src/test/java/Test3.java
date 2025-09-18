ec221c6767ac790dcd127e75b550f12565e5334a
jananirj94 <jananirj1794@gmail.com>
2025-09-18 16:22:22 +0530

    Commit the test3 as another name

---
+++ src/test/java/Test3.java
@@ -0,0 +1,14 @@
+
+public class Test3 {
+
+	public static void main(String[] args) {
+			String name="sakthi";
+			if(name.equals(name)) {
+				System.out.println("Name is sakthi");
+			}
+			else {
+				System.out.println("Name is not sakthi");
+			}
+	}
+
+}
