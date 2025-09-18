413aac54fa253a93afb198adc6cf949a7ea1460a
jananirj94 <jananirj1794@gmail.com>
2025-09-18 14:48:00 +0530

    Committed name as aardhana in Test2

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -2,12 +2,12 @@
 public class Test2 {
 
 	public static void main(String[] args) {
-			String name="sakthi";
+			String name="aardhana";
 			if(name.equals(name)) {
-				System.out.println("Name is sakthi");
+				System.out.println("Name is aardhana");
 			}
 			else {
-				System.out.println("Name is not sakthi");
+				System.out.println("Name is not aardhana");
 			}
 	}
 
