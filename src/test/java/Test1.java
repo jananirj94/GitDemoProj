7eb65531b4e6e09c6fedbde0e904168f74defad5
jananirj94 <jananirj1794@gmail.com>
2025-09-18 11:20:14 +0530

    First Commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+			String name="janani";
+			if(name.equals(name)) {
+				System.out.println("Name is Janani");
+			}
+			else {
+				System.out.println("Name is not janani");
+			}
+	}
+
+}
